1f3eaed78575b7fbf9625784a75e2d0c80e711f7
mkZ3R0 <bscs22021@itu.edu.pk>
2024-10-05 23:40:42 +0500

    Select a good color scheme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-blue-500 p-4">
+        <nav className="bg-gray-900 p-4">
             <div className="flex items-center justify-between">
                 <div className="text-white text-2xl font-bold">ReactTailWind</div>  {/* Logo */}
 
                 <div className="md:hidden">
-                    <button className="text-white" onClick={toggleMenu}>
+                    <button className="text-teal-400" onClick={toggleMenu}>
                         <svg
                             fill='none'
                             stroke='currentColor'
@@ -34,17 +34,17 @@ const Navbar = () => {
                 </div>
 
                 <ul className="hidden md:flex space-x-4 items-center">
-                    <li><a href="#" className='text-white px-3 py-2 rounded hover:bg-blue-700'>Home</a></li>
-                    <li><a href="#" className='text-white px-3 py-2 rounded hover:bg-blue-700'>Experiences</a></li>
-                    <li><a href="#" className='text-white px-3 py-2 rounded hover:bg-blue-700'>Online Experiences</a></li>
+                    <li><a href="#" className='text-white px-3 py-2 rounded hover:text-teal-400 duration-300'>Home</a></li>
+                    <li><a href="#" className='text-white px-3 py-2 rounded hover:text-teal-400 duration-300'>Experiences</a></li>
+                    <li><a href="#" className='text-white px-3 py-2 rounded hover:text-teal-400 duration-300'>Online Experiences</a></li>
                     <li className="relative">
-                        <button className="text-white px-3 py-2 rounded hover:bg-blue-700" onClick={toggleDropdown}>
+                        <button className="text-white px-3 py-2 rounded hover:text-teal-400 duration-300" onClick={toggleDropdown}>
                             My Profile
                         </button>
                         {isDropdownOpen && (
-                            <div className="absolute right-0 mt-2 text-center bg-blue-500 rounded-md shadow-lg py-2 pz-10">
-                                <a href="/signup" className="block px-4 py-2 text-white hover:bg-blue-700 rounded">Sign Up</a>
-                                <a href="/login" className="block px-4 py-2 text-white hover:bg-blue-700 rounded">Login</a>
+                            <div className="absolute right-0 mt-2 text-center bg-gray-900 rounded-md shadow-lg py-2 pz-10">
+                                <a href="/signup" className="block px-4 py-2 text-white hover:text-teal-400 duration-300 rounded">Sign Up</a>
+                                <a href="/login" className="block px-4 py-2 text-white hover:text-teal-400 duration-300 rounded">Login</a>
                             </div>
                         )}
                     </li>
@@ -54,17 +54,17 @@ const Navbar = () => {
             {/* Mobile Menu */}
             {isMenuOpen && (
                 <div className="md:hidden">
-                    <a href="#" className="block px-4 py-2 text-white hover:bg-blue-700">Home</a>
-                    <a href="#" className="block px-4 py-2 text-white hover:bg-blue-700">Experiences</a>
-                    <a href="#" className="block px-4 py-2 text-white hover:bg-blue-700">Online Experiences</a>
+                    <a href="#" className="block px-4 py-2 text-white hover:text-teal-400 duration-300">Home</a>
+                    <a href="#" className="block px-4 py-2 text-white hover:text-teal-400 duration-300">Experiences</a>
+                    <a href="#" className="block px-4 py-2 text-white hover:text-teal-400 duration-300">Online Experiences</a>
                     <div className="relative">
-                        <button className="block w-full text-left px-4 py-2 text-white hover:bg-blue-700" onClick={toggleDropdown}>
+                        <button className="block w-full text-left px-4 py-2 text-white hover:text-teal-400 duration-300" onClick={toggleDropdown}>
                             My Profile
                         </button>
                         {isDropdownOpen && (
-                            <div className="mt-0 w-full bg-blue-500 rounded-md shadow-lg py-2">
-                                <a href="/signup" className="block px-4 py-2 text-white hover:bg-blue-700 rounded">Sign Up</a>
-                                <a href="/login" className="block px-4 py-2 text-white hover:bg-blue-700 rounded">Login</a>
+                            <div className="mt-0 w-full bg-gray-900 rounded-md shadow-lg py-2">
+                                <a href="/signup" className="block px-4 py-2 text-white hover:text-teal-400 duration-300 rounded">Sign Up</a>
+                                <a href="/login" className="block px-4 py-2 text-white hover:text-teal-400 duration-300 rounded">Login</a>
                             </div>
                         )}
                     </div>
