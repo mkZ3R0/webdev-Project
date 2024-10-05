209c7df1bed26fe623e455392a6257908babbe0b
mkZ3R0 <bscs22021@itu.edu.pk>
2024-10-06 03:25:25 +0500

    Fix scrollable buttons UI bug

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -9,9 +9,9 @@ const CategoryList = () =>
         console.log(`Category Clicked ${name}`);
     };
     return (
-        <div className="flex flex-col justify-center max-w-full bg-transparent px-4 sm:px-6 lg:px-8 rounded">
+        <div className="flex flex-col justify-center max-w-full bg-transparent px-4 sm:px-6 lg:px-8">
 
-            <h1 className="text-2xl font-bold text-center mb-2 text-teal-400">Explore our Arsenal</h1>
+            <h1 className="text-2xl font-bold text-center my-2 text-teal-400">Explore our Arsenal</h1>
 
             <div>
                 <ul className="flex flex-row mb-3 gap-4 bg-gray-700 overflow-y-auto scrollbar-hide max-w-full">
